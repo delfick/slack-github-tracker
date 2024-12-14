5441ac739e9b4e2fe2a88acd91381ed4cbebf6c2
Stephen Moore <stephen@delfick.com>
2024-12-14 21:56:21 +1100

    Make it possible to make customized serve-http click command

--- slack_github_tracker/cli.py
+++ slack_github_tracker/cli.py
@@ -1,5 +1,7 @@
+import functools
 import logging
 import os
+from collections.abc import Callable
 from typing import Any
 
 import click
@@ -71,52 +73,18 @@ def setup_logging(dev_logging: bool) -> protocols.Logger:
     return log
 
 
-@click.command()
-@click.option(
-    "--slack-bot-token",
-    help="The value of the token for the slack bot or 'env:NAME_OF_ENV_VAR'",
-    default="env:SLACK_BOT_TOKEN",
-    type=EnvSecret(),
-)
-@click.option(
-    "--slack-signing-secret",
-    help="The value of the signing secret for the slack app or 'env:NAME_OF_ENV_VAR'",
-    default="env:SLACK_SIGNING_SECRET",
-    type=EnvSecret(),
-)
-@click.option(
-    "--github-webhook-secret",
-    help="The value of the secret for the github webhooks or 'env:NAME_OF_ENV_VAR'",
-    default="env:GITHUB_WEBHOOK_SECRET",
-    type=EnvSecret(),
-)
-@click.option(
-    "--postgres-url",
-    help="The url for the postgres database",
-    default="env:ALEMBIC_DB_URL",
-    type=EnvSecret(),
-)
-@click.option(
-    "--port",
-    help="The port to expose the app from. Defaults to $SLACK_BOT_SERVER_PORT or 3000",
-    default=os.environ.get("SLACK_BOT_SERVER_PORT", 3000),
-    type=int,
-)
-@click.option(
-    "--dev-logging",
-    is_flag=True,
-    help="Print out the logs as human readable",
-)
-def serve_http(
+def start_http_server(
+    *,
     slack_bot_token: str,
     slack_signing_secret: str,
     github_webhook_secret: str,
     postgres_url: str,
     port: int,
     dev_logging: bool,
+    server_kls: type[http_server.Server],
 ) -> None:
     logger = setup_logging(dev_logging)
-    server = http_server.Server(
+    server = server_kls(
         postgres_url=postgres_url,
         slack_bot_token=slack_bot_token,
         slack_signing_secret=slack_signing_secret,
@@ -125,6 +93,71 @@ def serve_http(
         logger=logger,
     )
     server.serve_forever()
+
+
+def http_server_args[**P_Args, T_Ret](func: Callable[P_Args, T_Ret]) -> Callable[P_Args, T_Ret]:
+    @click.option(
+        "--slack-bot-token",
+        help="The value of the token for the slack bot or 'env:NAME_OF_ENV_VAR'",
+        default="env:SLACK_BOT_TOKEN",
+        type=EnvSecret(),
+    )
+    @click.option(
+        "--slack-signing-secret",
+        help="The value of the signing secret for the slack app or 'env:NAME_OF_ENV_VAR'",
+        default="env:SLACK_SIGNING_SECRET",
+        type=EnvSecret(),
+    )
+    @click.option(
+        "--github-webhook-secret",
+        help="The value of the secret for the github webhooks or 'env:NAME_OF_ENV_VAR'",
+        default="env:GITHUB_WEBHOOK_SECRET",
+        type=EnvSecret(),
+    )
+    @click.option(
+        "--postgres-url",
+        help="The url for the postgres database",
+        default="env:ALEMBIC_DB_URL",
+        type=EnvSecret(),
+    )
+    @click.option(
+        "--port",
+        help="The port to expose the app from. Defaults to $SLACK_BOT_SERVER_PORT or 3000",
+        default=os.environ.get("SLACK_BOT_SERVER_PORT", 3000),
+        type=int,
+    )
+    @click.option(
+        "--dev-logging",
+        is_flag=True,
+        help="Print out the logs as human readable",
+    )
+    @functools.wraps(func)
+    def wrapped(*args: P_Args.args, **kwargs: P_Args.kwargs) -> T_Ret:
+        return func(*args, **kwargs)
+
+    return wrapped
+
+
+@click.command
+@http_server_args
+def serve_http(
+    *,
+    slack_bot_token: str,
+    slack_signing_secret: str,
+    github_webhook_secret: str,
+    postgres_url: str,
+    port: int,
+    dev_logging: bool,
+) -> None:
+    return start_http_server(
+        slack_bot_token=slack_bot_token,
+        slack_signing_secret=slack_signing_secret,
+        github_webhook_secret=github_webhook_secret,
+        postgres_url=postgres_url,
+        port=port,
+        dev_logging=dev_logging,
+        server_kls=http_server.Server,
+    )
 
 
 @click.group(help="Interact with slack github tracker")
