2c7a5096fc068c3756811fb6e778f40877999cc6
Stephen Moore <stephen@delfick.com>
2024-12-14 22:29:04 +1100

    Make it easy to turn off specific interpreters

--- slack_github_tracker/handlers/github/_interpret/_interpret.py
+++ slack_github_tracker/handlers/github/_interpret/_interpret.py
@@ -10,20 +10,21 @@ from . import _pull_request_review as pull_request_review
 
 @attrs.frozen
 class EventInterpreter:
-    pull_request: protocols.EventInterpreter = attrs.field(
+    pull_request: protocols.EventInterpreter | None = attrs.field(
         factory=pull_request.PullRequestEventInterpreter
     )
-    pull_request_review: protocols.EventInterpreter = attrs.field(
+    pull_request_review: protocols.EventInterpreter | None = attrs.field(
         factory=pull_request_review.PullRequestReviewEventInterpreter
     )
 
-    def __iter__(self) -> Iterator[protocols.EventInterpreter]:
+    def __iter__(self) -> Iterator[protocols.EventInterpreter | None]:
         yield self.pull_request
         yield self.pull_request_review
 
     def interpret(self, incoming: protocols.Incoming) -> Iterator[protocols.Event]:
         for interpreter in self:
-            yield from interpreter.interpret(incoming)
+            if interpreter is not None:
+                yield from interpreter.interpret(incoming)
 
 
 if TYPE_CHECKING:
